0901b43adac55a2e32eedce3675ab89269d2cc45
catherinev56 <51975176+catherinev56@users.noreply.github.com>
2019-06-19 09:41:46 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function setPenColour(color) {pixel.style.backgroundColor=color}
+var penColour='black';
+function setPenColour(pen){
+  penColour= pen;
+}
+function setPixelColour(pixel){
+  pixel.style.backgroundColor = penColour;
+}
